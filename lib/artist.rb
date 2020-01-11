1a2489f30d5febd81b312d0cf52795290b87df74
Joanna E K <kalemajoanna@gmail.com>
2020-01-11 13:21:33 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist 
+  @@all = []
+  attr_accessor :name, :song 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+    @songs = []
+    # @artist= artist 
+  end 
+  
+  # def artist=(artist)
+  #   @artist= artist
+  # end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def save 
+    @@all << name 
+  end 
+  
+  def self.destroy_all 
+    @@all = []
+  end 
+  
+  def self.create(name)
+    song = self.new(name)
+	song.save
+	song
+	end 
+	
+	def add_song(name)
+	  @name 
+	end 
+	
+	def songs 
+	  @songs 
+	end 
+	
+  
+  
+end 
---
+++ lib/genre.rb
@@ -0,0 +1,37 @@
+class Genre 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+    @songs = []
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def save 
+    @@all << name 
+  end 
+  
+  def self.destroy_all 
+    @@all = []
+  end 
+  
+  def self.create(name)
+    song = self.new(name)
+	song.save
+	song
+	end 
+	
+		def songs 
+	  @songs 
+	end 
+	
+	def genre 
+	  genre.song 
+	end 
+  
+end 
