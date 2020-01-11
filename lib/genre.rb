41f27d3e12c32959ac54b0ccc3e740259e6e47f8
Joanna E K <kalemajoanna@gmail.com>
2020-01-11 12:40:54 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,28 @@
+class Genre 
+  @@all = []
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def save 
+    @@all << name 
+  end 
+  
+  def self.destroy_all 
+    @@all = []
+  end 
+  
+  def self.create(name)
+    song = self.new(name)
+	song.save
+	song
+	end 
+  
+end 
